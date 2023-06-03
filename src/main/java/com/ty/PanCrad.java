c8ef207143e3bab4f0ebce295bd571004eea9ad0
unknown <geeta.s.rathodi@gmail.com>
2023-06-03 12:13:34 +0530

    person entity

--- src/main/java/com/ty/PanCrad.java
+++ src/main/java/com/ty/PanCrad.java
@@ -1,5 +1,47 @@
 package com.ty;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class PanCrad {
+	@Id
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	private String panno;
+	private String state;
+	private String country;
+	private String Address;
+	public String getPanno() {
+		return panno;
+	}
+	public void setPanno(String panno) {
+		this.panno = panno;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	
 
 }
--- src/main/java/com/ty/Person.java
+++ src/main/java/com/ty/Person.java
@@ -1,5 +1,34 @@
 package com.ty;
 
 public class Person {
+	private int id;
+	private String name;
+	private String address;
+	private long phno;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public long getPhno() {
+		return phno;
+	}
+	public void setPhno(long phno) {
+		this.phno = phno;
+	}
+	
 
 }
